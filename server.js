806ae6292f89be1b72bf46804bcabf0ce06570f8
RibasDEV22 <muriloribeirofidelis04@gmail.com>
2026-08-30 11:52:54 -0400

    Add stmtRemoveReaction to server.js

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const {
     stmtSetSetting,
     stmtMarkAsRead,
     stmtUpsertReaction,
+    stmtRemoveReaction,
     stmtGetReactionsByMessage,
     stmtUpdateLastSeen,
     stmtUpsertPushSubscription,
